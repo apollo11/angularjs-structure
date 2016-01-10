c4b63a4f56bb85176882b3aeae1b9eb13c55e788
apollo <apollo.malapote@mnltechnology.com>
2016-01-10 20:09:19 +0800

    First commit

---
+++ app/app.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var app = angular.module('myApp', [
+    'ui.router',
+    'ngResource'
+]);
---
+++ app/components/test/testController.js
@@ -0,0 +1,7 @@
+/**
+ * Created by apollo on 10/01/2016.
+ */
+app.controller('TestController', function($scope) {
+
+    $scope.test = 'test';
+});
